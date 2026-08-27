70650c719ce2e46b6a61662c92618212cd2ecb40
anupamme <mediratta@gmail.com>
2026-08-27 20:39:26 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- web_interface/static/js/browser-ml/umap-wasm/src/memory.cpp
+++ web_interface/static/js/browser-ml/umap-wasm/src/memory.cpp
@@ -1,8 +1,21 @@
 // Copyright (c) 2025 Apple Inc. Licensed under MIT License.
 
 #include "common.hpp"
+#include <cstdlib>
+
+// Cap allocations to the wasm32 address space to reject bogus/huge sizes.
+static constexpr size_t kMaxAllocationSize = static_cast<size_t>(1) << 32;
 
 extern "C" {
-APIFUNC void *memory_allocate(size_t size) { return malloc(size); }
-APIFUNC void memory_free(void *ptr) { free(ptr); }
+APIFUNC void *memory_allocate(size_t size) {
+  if (size == 0 || size > kMaxAllocationSize) {
+    return nullptr;
+  }
+  return malloc(size);
+}
+APIFUNC void memory_free(void *ptr) {
+  if (ptr != nullptr) {
+    free(ptr);
+  }
+}
 }
